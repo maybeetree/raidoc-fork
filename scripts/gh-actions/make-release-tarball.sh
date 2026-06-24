f705c2f8d359499a37dd75a9e67d7ecb899b74ea
maybetree <maybetree48@proton.me>
2026-06-24 15:08:03 +0200

    add release generation

---
+++ scripts/gh-actions/make-release-tarball.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+tagname=$(git describe --tags --abbrev=0)
+mv "build" "raidoc-${tagname}-web"
+tar cf - "raidoc-${tagname}-web" | \
+	zstd --compress --ultra -20 --threads=0 - -o "raidoc-${tagname}-web.tar.zst"
+mv "raidoc-${tagname}-web" "build"
+ls -lah
+set +e
+
